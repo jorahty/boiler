d823b02cb2ee309900d826a5b9ffe875704908b5
James Tennant <jtennant@ucsc.edu>
2023-09-01 15:20:29 -0700

    update favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import '@radix-ui/themes/styles.css';
 import type { AppProps } from 'next/app';
 import { Theme } from '@radix-ui/themes';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <title>Boiler</title>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
